3bb3c4b925f7fd674c887aafb0490f6a671ad144
zombiegirlcz <zombiegirlcz@gmail.com>
2026-09-01 14:34:48 +0000

    Fix temp tar file creation using mktemp without -u

--- setup-test.sh
+++ setup-test.sh
@@ -6,7 +6,7 @@ UBUNTU_CODENAME="bionic"
 UBUNTU_VERSION="18.04.5"
 BASE_URL="https://cdimage.ubuntu.com/ubuntu-base/releases/${UBUNTU_VERSION}/release/ubuntu-base-${UBUNTU_VERSION}-base-${ARCH}.tar.gz"
 ROOTFS_DIR="$(cd "$(dirname "$0")" && pwd)/testrootfs"
-TMP_TAR="$(mktemp -u).tar.gz"
+TMP_TAR="$(mktemp)"
 
 cleanup() {
   rm -f "$TMP_TAR"
